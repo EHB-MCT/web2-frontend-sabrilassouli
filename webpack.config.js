2384e3e1ed1689624adcbbb883f991b5fe48c514
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-12-24 05:36:42 +0100

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'docs'),
 
+module.exports = {
+    // entry: ['./src/index.js', './src/savedInput.js'],
+    entry: {
+        index: './src/index.js',
+        input: './src/getMeals.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'main.js'
     },
     mode: 'development'
-};
+}
